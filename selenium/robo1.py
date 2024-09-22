c222e3042f1673a5b404e04bc4e76227fa7894a4
mconrado <mconrado@protonmail.com>
2024-09-21 21:29:29 -0300

    leitura de xls

--- selenium/robo1.py
+++ selenium/robo1.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import xlrd
 
 # Configurar o ChromeDriver
 options = webdriver.ChromeOptions()
@@ -11,12 +12,19 @@ options.binary_location = '/usr/bin/chromium-browser'  # Substitua pelo caminho 
 
 print("Iniciando nosso robô...\n")
 
+dominios = []
+#lendo excel
+workbook = xlrd.open_workbook('dominio.xls')
+sheet = workbook.sheet_by_index(0)
+
+for linha in range(0, 10):
+    dominios.append(sheet.cell_value(linha, 0))
+
 # Inicia o WebDriver usando o webdriver_manager
 service = Service(ChromeDriverManager(driver_version="112.0.5615.49").install())
 driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://registro.br/")
 
-dominios = ["roboscompython.com.br", "udemy.com", "uol.com.br", "pythoncursos.com.br"]
 for dominio in dominios:
     pesquisa = driver.find_element(By.ID, "is-avail-field")
     pesquisa.clear()
