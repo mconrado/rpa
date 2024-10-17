487b820a39a77b61a8bbdae4140f274beaf42dd6
mconrado <mconrado@protonmail.com>
2024-10-17 10:55:05 -0300

    salvando resultados em um txt

--- google/script.py
+++ google/script.py
@@ -29,24 +29,35 @@ resultados = driver.find_element(By.XPATH, "//*[@id='result-stats']").text
 print(resultados)
 numero_resultados = int(resultados.split("Aproximadamente")[1].split(' resultados')[0].replace('.', ''))
 maximo_paginas = numero_resultados/10
-print("Número de páginas: %s" % (maximo_paginas))
-
+pagina_alvo = input("%s páginas encontradas, até qual página quer ir?" % (maximo_paginas))
+lista_resultados = []
 
 url_pagina = driver.find_element(By.XPATH, "//a[@aria-label='Page 2']").get_attribute("href")
 
 pagina_atual = 0
 start = 10
-while pagina_atual < 10:
-    if not pagina_atual == 0:
+
+while pagina_atual <= int(pagina_alvo) -1:
+    if pagina_atual > 1:
         url_pagina = url_pagina.replace("start=%s" % start, "start=%s" % (start+10))
         start += 10
+        driver.get(url_pagina)
+    elif pagina_atual == 1:
+        driver.get(url_pagina)
     pagina_atual += 1
-    driver.get(url_pagina)
 
+    time.sleep(2)
 
-divs = driver.find_elements(By.XPATH, "//div[@class='MjjYud']")
-for div in divs:
-    nome = div.find_element(By.TAG_NAME, 'span')
-    link = div.find_element(By.TAG_NAME, 'a')
-    resultado = "%s;%s" % (nome.text, link.get_attribute("href"))
-    print(resultado)
+    divs = driver.find_elements(By.XPATH, "//div[@class='yuRUbf']")
+    for div in divs:
+        nome = div.find_element(By.TAG_NAME, 'span')
+        link = div.find_element(By.TAG_NAME, 'a')
+        resultado = "%s;%s" % (nome.text, link.get_attribute("href"))
+        print(resultado)
+        lista_resultados.append(resultado)
+
+with open("resultados.txt", "w") as arquivo:
+    for resultado in lista_resultados:
+        arquivo.write("%s\n" % resultado)
+    arquivo.close()
+print("%s resultados encontrados no Google e salvos no arquivo.txt" % len(lista_resultados))
