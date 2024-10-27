50ac60a9ccecda9ec7fc68c1f32a45a1bf09a12e
mconrado <mconrado@protonmail.com>
2024-10-27 14:11:47 -0300

    trazendo todos os resultados fixados em páginas

---
+++ youtube/README.md
@@ -0,0 +1,10 @@
+
+# Robo simples resgatando resultados Youtube
+
+Exercício adaptado para utilizar a biblioteca Selenium 4.24 e usar o gerenciamento de driver para automatizar o navegador já instalado sem a necessidade de baixar o driver.
+
+
+#### Requisitos
+```
+pip install -r requirements.txt
+python bot.py
--- youtube/bot.py
+++ youtube/bot.py
@@ -20,15 +20,31 @@ class RoboYoutube():
     def __init__(self):
         self.webdriver = webdriver.Chrome(service=service, options=options)
 
-    def busca(self, busca):
+    def busca(self, busca, paginas):
+        videos = []
+        pagina = 1
+
         url = "https://www.youtube.com/results?search_query=%s" % busca
         self.webdriver.get(url)
-        titulos = self.webdriver.find_elements(By.XPATH, "//a[@id='video-title']")
-        for titulo in titulos:
-            print("Vídeo encontrado: %s" % titulo.text)
+
+        while pagina <= paginas:
+            titulos = self.webdriver.find_elements(By.XPATH, "//a[@id='video-title']")
+            for titulo in titulos:
+                if titulo.text not in videos:
+                    print("Vídeo encontrado: %s" % titulo.text)
+                    videos.append(titulo.text)
+            self.proxima_pagina(pagina)
+            pagina += 1
+
+    def proxima_pagina(self, pagina):
+        print("Mudando para a página %s" % (pagina + 1))
+        bottom = pagina * 10000
+        self.webdriver.execute_script("window.scrollTo(0, %s);" % bottom)
+        time.sleep(5)
+        pass
 
 if __name__ == "__main__":
     bot = RoboYoutube()
-    bot.busca("teste")
+    bot.busca("teste", 2)
     time.sleep(3)
 
