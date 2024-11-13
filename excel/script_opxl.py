fc2b00c1a2efb39abcec03f0a9726852b53af92d
mconrado <mconrado@protonmail.com>
2024-11-13 20:16:15 -0300

    inserindo operações xlsx

---
+++ excel/script_opxl.py
@@ -0,0 +1,11 @@
+from openpyxl import load_workbook
+
+wb = load_workbook(filename="nomes2.xlsx")
+
+planilha = wb["Planilha1"]
+
+for i in range(2, 5):
+    print("%s tem %s anos." % (planilha["A%s" % i].value, planilha["B%s" % i].value))
+
+planilha["B5"] = "=SUM(B2:B4)"
+wb.save(filename="nomes2.xlsx")
