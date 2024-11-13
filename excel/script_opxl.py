e5bf079657856c66ede36ab00c8596babbe42a9b
mconrado <mconrado@protonmail.com>
2024-11-13 20:29:57 -0300

    mesclando celulas

--- excel/script_opxl.py
+++ excel/script_opxl.py
@@ -8,4 +8,10 @@ for i in range(2, 5):
     print("%s tem %s anos." % (planilha["A%s" % i].value, planilha["B%s" % i].value))
 
 planilha["B5"] = "=SUM(B2:B4)"
+
+planilha["A7"] = "ALUNOS"
+planilha.merge_cells("A7:B7")
+planilha.unmerge_cells("A7:B7")
+
+
 wb.save(filename="nomes2.xlsx")
