8b4bef377b590e068e6a6c8caeb7b03c7c3a3b63
mconrado <mconrado@protonmail.com>
2024-11-24 09:47:05 -0300

    listando os grupos

--- api/TelegramBot.py
+++ api/TelegramBot.py
@@ -22,6 +22,35 @@ class TelegramBot:
             await self.client.sign_in(self.phone, input("Digite o código: "))
             return True
 
+    async def get_groups(self):
+        groups = []
+        chats = await self.client(
+            GetDialogsRequest(
+                offset_date=None,
+                offset_id=0,
+                offset_peer=InputPeerEmpty(),
+                limit=200,
+                hash=0,
+            )
+        )
+
+        for chat in chats.chats:
+            try:
+                if chat.megagroup == True:
+                    groups.append(chat)
+            except:
+                continue
+
+        print("Escolha um grupo")
+        i = 0
+        for group in groups:
+            print(str(i) + " - " + group.title)
+            i += 1
+
+        escolha = input("Escolha um número: ")
+        grupo_alvo = groups[int(escolha)]
+        return
+
     async def initialize(self):
         await self.connect()
         print("Conectado ao Telegram!")
--- api/run.py
+++ api/run.py
@@ -5,6 +5,7 @@ from TelegramBot import TelegramBot
 async def main():
     obj_telegram = TelegramBot()
     await obj_telegram.initialize()  # Chama o método de inicialização
+    await obj_telegram.get_groups()
 
 
 if __name__ == "__main__":
